2cc26fb2cac2379a9fa85b022f6af2f1c4e18b6d
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2023-01-17 15:32:51 +0500

    Update Scrape1.py
    inserting tags and working with find command

--- Scrape1.py
+++ Scrape1.py
@@ -1,4 +1,8 @@
 from bs4 import BeautifulSoup
+
 with open('D:\FAST UNIVERSITY\SEMESTER !\ICT LAB\Assignment 6\html form.html','r') as html_file:
     content = html_file.read()
-    print(content)
+    soup = BeautifulSoup(content , 'lxml')
+
+    tags = soup.find('input')
+    print(tags)
