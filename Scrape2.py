4c7587859b1e720905f7b8f6e03a731c1f23775f
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2023-01-17 17:00:45 +0500

    Update Scrape2.py
    soup update

--- Scrape2.py
+++ Scrape2.py
@@ -2,4 +2,6 @@ from bs4 import BeautifulSoup
 import requests
 
 html_url = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=PYTHON&txtLocation=').text  #enter url of website
-print(html_url)
+# print(html_url)
+
+soup = BeautifulSoup(html_url , 'lxml')
