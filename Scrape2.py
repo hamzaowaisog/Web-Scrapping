f4c3d631d9cc918e32b2c5de6222fb382f6b4b67
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2023-01-17 16:58:19 +0500

    Update Scrape2.py
    enhancing with .text method to get the code from it

--- Scrape2.py
+++ Scrape2.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_url = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=PYTHON&txtLocation=')  #enter url of website
+html_url = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=PYTHON&txtLocation=').text  #enter url of website
 print(html_url)
