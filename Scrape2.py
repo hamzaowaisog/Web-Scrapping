a3931a20273b058f8d786ddc94de5aeee071c9bf
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2023-01-19 15:40:19 +0500

    update v2.0
    Including skills

--- Scrape2.py
+++ Scrape2.py
@@ -6,6 +6,9 @@ html_url = requests.get('https://www.timesjobs.com/candidate/job-search.html?sea
 
 soup = BeautifulSoup(html_url , 'lxml')
 jobs = soup.find('li',class_ = 'clearfix job-bx wht-shd-bx')
-# print(jobs)
+
 company_name  = jobs.find('h3',class_ ='joblist-comp-name').text.replace(' ','')
+skills = jobs.find('span', class_ = 'srp-skills').text.replace(' ','')
+# print(jobs)
+print(skills)
 print(company_name)
