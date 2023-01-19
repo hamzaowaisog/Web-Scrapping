1192dba26a1d9612784b068c161a7ff2b87b5cb8
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2023-01-19 16:15:01 +0500

    Update Scrape2.py
    stopping time to scrap the data for every 10 minute

--- Scrape2.py
+++ Scrape2.py
@@ -1,24 +1,34 @@
+import time
+
 from bs4 import BeautifulSoup
 import requests
+import time
 
 print("Put some skills that you are not familiar with ")
 unfamiliar_skill = input('>')
 print(f"Filterting out : {unfamiliar_skill}")
+def find_job():
+    html_url = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=PYTHON&txtLocation=').text  #enter url of website
+    # print(html_url)
 
-html_url = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=PYTHON&txtLocation=').text  #enter url of website
-# print(html_url)
+    soup = BeautifulSoup(html_url , 'lxml')
+    job = soup.find_all('li',class_ = 'clearfix job-bx wht-shd-bx')
 
-soup = BeautifulSoup(html_url , 'lxml')
-job = soup.find_all('li',class_ = 'clearfix job-bx wht-shd-bx')
+    for jobs in job :
+        publish_date = jobs.find('span', class_='sim-posted').span.text
+        if 'few' in publish_date:
+            company_name  = jobs.find('h3',class_ ='joblist-comp-name').text.replace(' ','')
+            skills = jobs.find('span', class_ = 'srp-skills').text.replace(' ','')
+            more_info = jobs.header.h2.a['href']
+            if unfamiliar_skill not in skills:
+                print(f"Company Name : {company_name.strip()}")
+                print(f"Required skills : {skills.strip()}")
+                print(f"More info : {more_info}")
+                print()
 
-for jobs in job :
-    publish_date = jobs.find('span', class_='sim-posted').span.text
-    if 'few' in publish_date:
-        company_name  = jobs.find('h3',class_ ='joblist-comp-name').text.replace(' ','')
-        skills = jobs.find('span', class_ = 'srp-skills').text.replace(' ','')
-        more_info = jobs.header.h2.a['href']
-        if unfamiliar_skill not in skills:
-            print(f"Company Name : {company_name.strip()}")
-            print(f"Required skills : {skills.strip()}")
-            print(f"More info : {more_info}")
-            print()
+if __name__ == '__main__':
+    while True:
+        find_job()
+        time_wait = 10
+        print(f"Waiting {time_wait} minutes......")
+        time.sleep(time_wait *60)
