162e2c03ab1827c54214dbb31efe79c0a3ae6360
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2023-01-19 15:52:41 +0500

    Update Scrape2.py
    including if statement for few days job and printing

--- Scrape2.py
+++ Scrape2.py
@@ -8,14 +8,16 @@ soup = BeautifulSoup(html_url , 'lxml')
 job = soup.find_all('li',class_ = 'clearfix job-bx wht-shd-bx')
 
 for jobs in job :
-    company_name  = jobs.find('h3',class_ ='joblist-comp-name').text.replace(' ','')
-    skills = jobs.find('span', class_ = 'srp-skills').text.replace(' ','')
-    publish_date = jobs.find('span', class_ = 'sim-posted').span.text
-    # print(jobs)
-    # print(skills)
-    # print(company_name)
+    publish_date = jobs.find('span', class_='sim-posted').span.text
+    if 'few' in publish_date:
+        company_name  = jobs.find('h3',class_ ='joblist-comp-name').text.replace(' ','')
+        skills = jobs.find('span', class_ = 'srp-skills').text.replace(' ','')
 
-    print(f'''
-    Company Name : {company_name}
-    Required Skills : {skills}''')
-    print('')
+        # print(jobs)
+        # print(skills)
+        # print(company_name)
+
+        print(f'''
+        Company Name : {company_name}
+        Required Skills : {skills}''')
+        print('')
