966a6c512cf0018d4ee0801e9b9f59e994af67ba
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2023-01-19 15:32:01 +0500

    Update Scrape2.py
    including company name
    replacing whitespaces
    including classes

--- Scrape2.py
+++ Scrape2.py
@@ -5,3 +5,7 @@ html_url = requests.get('https://www.timesjobs.com/candidate/job-search.html?sea
 # print(html_url)
 
 soup = BeautifulSoup(html_url , 'lxml')
+jobs = soup.find('li',class_ = 'clearfix job-bx wht-shd-bx')
+# print(jobs)
+company_name  = jobs.find('h3',class_ ='joblist-comp-name').text.replace(' ','')
+print(company_name)
